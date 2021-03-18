104b5f11a9aec060fcc72ac69651fb586760a8d4
Toyeeb <toyeeb@flutterwavego.com>
2021-03-18 16:58:03 +0100

    layout ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+  cities = [
+    {
+      name: 'Amsterdam',
+      image: 'assets/images/cities/amsterdam.jpg'
+    } ,
+    {
+      name: 'Paris',
+      image: 'assets/images/cities/paris.jpg'
+
+    } ,
+    {
+      name: 'London',
+      image: 'assets/images/cities/london.webp'
+
+    } ,
+    {
+      name: 'Berlin',
+      image: 'assets/images/cities/berlin.jpg'
+
+    } ,
+    {
+      name: 'Rome',
+      image: 'assets/images/cities/rome.jpg'
+
+    } ,
+  ] ;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
